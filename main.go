729eaee95e9ebf91d4777a3ab6223b5dda0c7f6e
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2023-06-04 20:23:36 +0300

    03-Variable Conversation

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 )
 
 func main() {
-	a := 42
-	fmt.Printf("Value : %d\t Binary Value: %b\t Hexadecimal: %#x\n", a, a, a)
+	a := 34
+	b := 42.0
+
+	fmt.Printf("%v\n", a*int(b)) // We can't multiply different types so we need to convert one of them
+	fmt.Printf("%v of type %T\n", a, a)
+	fmt.Printf("%v of type %T\n", b, b)
+
+	//Conversion
+	c := int(b)
+	fmt.Printf("%v of type %T\n", c, c)
+	b = float64(a)
+	fmt.Printf("%v of type %T\n", b, b)
+
 }
